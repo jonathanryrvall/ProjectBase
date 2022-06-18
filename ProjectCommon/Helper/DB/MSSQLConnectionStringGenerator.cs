ae84647a9982d15ac0e6f5f4e9af37ae5f8ff44c
Skroint <46086303+Skroint@users.noreply.github.com>
2022-06-18 11:03:47 +0200

    - MSSQL connection stirng generator

---
+++ ProjectCommon/Helper/DB/MSSQLConnectionStringGenerator.cs
@@ -0,0 +1,27 @@
+﻿using ProjectCommon.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCommon.Helper.DB
+{
+   
+
+    /// <summary>
+    /// Generates a connection string to a MSSQL database based on a <see cref="DBConfig"/>
+    /// </summary>
+    public class MSSQLConnectionStringGenerator
+    {
+        /// <summary>
+        /// Returns the connection string generated
+        /// </summary>
+        public string GetConnectionString(DBConfig dbConfig)
+        {
+            return $"Data Source={dbConfig.Server};Initial Catalog={dbConfig.DatabaseName};Persist Security Info=True;User ID={dbConfig.UserID};Password={dbConfig.Password};";
+        }
+    }
+
+ 
+}
